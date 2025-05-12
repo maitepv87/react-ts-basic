3981397c8272b080a7942952fcb35dc7206376bc
Maite Perez Vento <maitepv87@gmail.com>
2025-05-12 11:29:16 -0400

    Add InfoBox component

--- src/components/GoalList.tsx
+++ src/components/GoalList.tsx
@@ -1,5 +1,7 @@
+import { type ReactNode } from "react";
 import { GoalItem } from "./GoalItem.tsx";
 import { GoalItem as CGol } from "../App.tsx";
+import { InfoBox } from "./InfoBox.tsx";
 
 // type GoalListPros = {
 //   goals: { title: string; description: string; id: number }[];
@@ -11,15 +13,35 @@ type GoalListPros = {
 };
 
 export const GoalList = ({ goals, onDeleteGoal }: GoalListPros) => {
+  if (goals.length === 0) {
+    return (
+      <InfoBox mode="hint">You have no goals yet. Start adding some!</InfoBox>
+    );
+  }
+
+  let warningBox: ReactNode;
+
+  if (goals.length >= 3) {
+    warningBox = (
+      <InfoBox mode="warning" severity="medium">
+        You are collecting a lot of goals!
+      </InfoBox>
+    );
+  }
+
   return (
-    <ul>
-      {goals.map((goal) => (
-        <li key={goal.id}>
-          <GoalItem title={goal.title} id={goal.id} onDelete={onDeleteGoal}>
-            <p>{goal.description}</p>
-          </GoalItem>
-        </li>
-      ))}
-    </ul>
+    <>
+      {warningBox}
+
+      <ul>
+        {goals.map((goal) => (
+          <li key={goal.id}>
+            <GoalItem title={goal.title} id={goal.id} onDelete={onDeleteGoal}>
+              <p>{goal.description}</p>
+            </GoalItem>
+          </li>
+        ))}
+      </ul>
+    </>
   );
 };
